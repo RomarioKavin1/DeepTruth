d037ed59ad7b7d58a8daa156880783de6d73d3fc
RomarioKavin1 <romario7kavin@gmail.com>
2025-04-05 11:54:10 +0800

    route_for_worldCoin

--- frontend/app/api/verify/route.ts
+++ frontend/app/api/verify/route.ts
@@ -15,7 +15,7 @@ interface IRequestPayload {
 export async function POST(req: NextRequest) {
   const { payload, action, signal, params } =
     (await req.json()) as IRequestPayload;
-  const app_id = process.env.APP_ID as `app_${string}`;
+  const app_id = process.env.NEXTAUTH_WORLDCOIN_ID as `app_${string}`;
   console.log("Signal", signal);
   console.log({
     app_id,
@@ -24,31 +24,27 @@ export async function POST(req: NextRequest) {
     signal,
     params,
   });
-  return NextResponse.json({
-    success: {
-      app_id,
-      payload,
-      action,
-      signal,
-    },
-    status: 200,
-  });
-
-  //   const verifyRes = (await verifyCloudProof(
-  //     payload,
-  //     app_id,
-  //     action,
-  //     signal
-  //   )) as IVerifyResponse; // Wrapper on this
-  //   console.log("Verify res");
-  //   console.log(verifyRes);
-  //   if (verifyRes.success) {
-  //     // This is where you should perform backend actions if the verification succeeds
-  //     // Such as, setting a user as "verified" in a database
-  //     return NextResponse.json({ success: verifyRes.success, status: 200 });
-  //   } else {
-  //     // This is where you should handle errors from the World ID /verify endpoint.
-  //     // Usually these errors are due to a user having already verified.
-  //     return NextResponse.json({ success: false, status: 400 });
-  //   }
+  const verifyRes = (await verifyCloudProof(
+    payload,
+    app_id,
+    action,
+    signal
+  )) as IVerifyResponse; // Wrapper on this
+  console.log("Verify res");
+  console.log(verifyRes);
+  if (verifyRes.success) {
+    // This is where you should perform backend actions if the verification succeeds
+    // Such as, setting a user as "verified" in a database
+    return NextResponse.json({
+      success: {
+        app_id,
+        payload,
+        action,
+        signal,
+      },
+      status: 200,
+    });
+  } else {
+    return NextResponse.json({ success: false, status: 400 });
+  }
 }
